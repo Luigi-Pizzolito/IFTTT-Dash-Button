a9f6c06180637e223c9e670e3e173d79e7383152
Luigi Pizzolito <luigi.pizzolito@hotmail.com>
2018-02-20 14:02:27 +0800

    Added some extra configurations!

--- IFTTT-Dash-Button.ino
+++ IFTTT-Dash-Button.ino
@@ -22,13 +22,16 @@ String ssid;
 String password;
 String host;
 String url;
-int wc_p;                  // max. time in seconds to connect to wifi, before giving up
-int gr_p;                  // max. times of attemps to perform GET request, before giving up
-bool s_vcc;              //wether to send VCC voltage as a parameter in the url request.
+int wc_p;        // max. time in seconds to connect to wifi, before giving up
+int gr_p;        // max. times of attemps to perform GET request, before giving up
+bool s_vcc;      //wether to send VCC voltage as a parameter in the url request.
+bool is_ip;      //wether host adress is IP
+String vcc_parm; //parameter to pass VCC voltage by.
 
 /*
    System Variables
 */
+//#define NOT_DEBUG //wether to enable debug or to show indication lights instead
 //Normal Mode
 int failCount = 0;
 ADC_MODE(ADC_VCC);
@@ -40,7 +43,8 @@ File fsUploadFile;
 const char *APssid = "ESP_Button";
 const char *APpass = "wifibutton";
 
-void setup() {
+void setup()
+{
   //start serial monitor, SPIFFS and Config. Pin
   Serial.begin(115200);
   pinMode(CONFIG_PIN, INPUT_PULLUP);
@@ -51,7 +55,8 @@ void setup() {
   Serial.println("SPIFFS Content: ");
   {
     Dir dir = SPIFFS.openDir("/");
-    while (dir.next()) {    
+    while (dir.next())
+    {
       String fileName = dir.fileName();
       size_t fileSize = dir.fileSize();
       Serial.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
@@ -64,15 +69,21 @@ void setup() {
 
   //read Config. Pin
   su_mode = !digitalRead(CONFIG_PIN);
-  if (su_mode) {
-    //start Config. Mode
+  if (su_mode)
+  {
+
+//start Config. Mode
+#ifdef NOT_DEBUG
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+#endif
     Serial.println("Entering Config. Mode!");
 
     //start WiFi Access Point
     Serial.println("Configuring access point...");
     WiFi.mode(WIFI_AP_STA);
     WiFi.softAP(APssid, APpass);
-  
+
     IPAddress myIP = WiFi.softAPIP();
     Serial.print("AP IP address: ");
     Serial.println(myIP);
@@ -81,23 +92,27 @@ void setup() {
 
     //edit pages
     server.on("/list", HTTP_GET, handleFileList);
-    server.on("/edit", HTTP_GET, [](){
-      if(!handleFileRead("/edit.htm")) server.send(404, "text/plain", "FileNotFound");
+    server.on("/edit", HTTP_GET, []() {
+      if (!handleFileRead("/edit.htm"))
+        server.send(404, "text/plain", "FileNotFound");
     });
     server.on("/edit", HTTP_PUT, handleFileCreate);
     server.on("/edit", HTTP_DELETE, handleFileDelete);
-    server.on("/edit", HTTP_POST, [](){ server.send(200, "text/plain", ""); }, handleFileUpload);
+    server.on("/edit", HTTP_POST, []() { server.send(200, "text/plain", ""); }, handleFileUpload);
 
     //pages from SPIFFS
-    server.onNotFound([](){
-      if(!handleFileRead(server.uri())) {
+    server.onNotFound([]() {
+      if (!handleFileRead(server.uri()))
+      {
         server.send(404, "text/plain", "FileNotFound");
       }
     });
 
     server.begin();
     Serial.println("HTTP server started");
-  } else {
+  }
+  else
+  {
     //start Normal Mode
 
     //connect to WiFi
@@ -109,12 +124,14 @@ void setup() {
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid.c_str(), password.c_str());
 
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
       delay(500);
       Serial.print(".");
       //if we are taking too long to connect to WiFi give up.
       failCount++;
-      if (failCount == wc_p * 2) {
+      if (failCount == wc_p * 2)
+      {
         Serial.println("Session Terminated. Giving up after 21 tries connecting to WiFi.");
         Serial.println("entering deep sleep");
         delay(20);
@@ -131,13 +148,18 @@ void setup() {
   }
 }
 
-void loop() {
-  if (su_mode) {
+void loop()
+{
+  if (su_mode)
+  {
     server.handleClient();
-  } else {
+  }
+  else
+  {
     //if we have tried too many times to make a GET Request give up.
     ++failCount;
-    if (failCount == gr_p + 1) {
+    if (failCount == gr_p + 1)
+    {
       Serial.println("Session Terminated. Giving up after 10 tries doing GET Request.");
       Serial.println("entering deep sleep");
       delay(20);
@@ -153,16 +175,43 @@ void loop() {
     //try to connect to the host with TCP
     WiFiClient client;
     const int httpPort = 80;
-    if (!client.connect(host.c_str(), httpPort)) {
-      //try again if the connection fails.
-      Serial.println("connection failed");
-      delay(10);
-      return;
+    if (is_ip)
+    {
+      IPAddress addr;
+      if (addr.fromString(host))
+      {
+        if (!client.connect(addr, httpPort))
+        {
+          //try again if the connection fails.
+          Serial.println("connection to IP failed");
+          delay(10);
+          return;
+        }
+      }
+      else
+      {
+        Serial.println("failed to convert IP String to IP Address.");
+        while (1)
+          ;
+      }
+    }
+    else
+    {
+      if (!client.connect(host.c_str(), httpPort))
+      {
+        //try again if the connection fails.
+        Serial.println("connection failed");
+        delay(10);
+        return;
+      }
     }
 
     //create the URI for the request
-    if (s_vcc) {
-      url += "?batt=";
+    if (s_vcc)
+    {
+      url += "?";
+      url += vcc_parm;
+      url += "=";
       uint32_t getVcc = ESP.getVcc();
       String VccVol = String((getVcc / 1000U) % 10) + "." + String((getVcc / 100U) % 10) + String((getVcc / 10U) % 10) + String((getVcc / 1U) % 10);
       url += VccVol;
@@ -177,8 +226,10 @@ void loop() {
                  "Connection: close\r\n\r\n");
 
     unsigned long timeout = millis();
-    while (client.available() == 0) {
-      if (millis() - timeout > 60000) {
+    while (client.available() == 0)
+    {
+      if (millis() - timeout > 60000)
+      {
         //give up if the server takes too long to reply.
         Serial.println(">>> Client Timeout !");
         client.stop();
@@ -188,9 +239,9 @@ void loop() {
       }
     }
 
-
     //print response to serial
-    while (client.available()) {
+    while (client.available())
+    {
       String line = client.readStringUntil('\r');
       Serial.print(line);
     }
@@ -211,7 +262,8 @@ void loop() {
    Universal Functions
 */
 
-void fail() {
+void fail()
+{
   //something has gone wrong, blink an indicator on the LED.
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
@@ -239,7 +291,8 @@ void fail() {
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
-void yay() {
+void yay()
+{
   //everything worked, blink an indicator on the LED.
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
@@ -251,25 +304,32 @@ void yay() {
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
-void readConfig() {
+void readConfig()
+{
   //read config.json and load configuration to variables.
-    File configFile = SPIFFS.open("/config.jsn", "r");
-  if (!configFile) {
+  File configFile = SPIFFS.open("/config.jsn", "r");
+  if (!configFile)
+  {
     Serial.println("Failed to open config file");
-    while(1);
+    while (1)
+      ;
   }
   size_t size = configFile.size();
-  if (size > 1024) {
+  if (size > 1024)
+  {
     Serial.println("Config file size is too large");
-    while(1);
+    while (1)
+      ;
   }
   std::unique_ptr<char[]> buf(new char[size]);
   configFile.readBytes(buf.get(), size);
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& json = jsonBuffer.parseObject(buf.get());
-  if (!json.success()) {
+  StaticJsonBuffer<300> jsonBuffer;
+  JsonObject &json = jsonBuffer.parseObject(buf.get());
+  if (!json.success())
+  {
     Serial.println("Failed to parse config file");
-    while(1);
+    while (1)
+      ;
   }
 
   ssid = (const char *)json["ssid"];
@@ -278,7 +338,9 @@ void readConfig() {
   url = (const char *)json["uri"];
   wc_p = json["wc_p"];
   gr_p = json["gr_p"];
-  s_vcc = json["s_vcc"]; 
+  s_vcc = json["s_vcc"];
+  is_ip = json["is_ip"];
+  vcc_parm = (const char *)json["vcc_p"];
 
   Serial.println("Parsed JSON Config.");
   Serial.print("Loaded ssid: ");
@@ -287,6 +349,8 @@ void readConfig() {
   Serial.println(password);
   Serial.print("Loaded host: ");
   Serial.println(host);
+  Serial.print("Loaded IsIP: ");
+  Serial.println(is_ip);
   Serial.print("Loaded uri: ");
   Serial.println(url);
   Serial.print("Loaded WiFi Connect Persistance: ");
@@ -295,6 +359,8 @@ void readConfig() {
   Serial.println(gr_p);
   Serial.print("Loaded Send VCC: ");
   Serial.println(s_vcc);
+  Serial.print("Loaded VCC Param.: ");
+  Serial.println(vcc_parm);
   Serial.println();
 }
 
@@ -303,49 +369,83 @@ void readConfig() {
 */
 
 //edit functions
-String formatBytes(size_t bytes){
-  if (bytes < 1024){
-    return String(bytes)+"B";
-  } else if(bytes < (1024 * 1024)){
-    return String(bytes/1024.0)+"KB";
-  } else if(bytes < (1024 * 1024 * 1024)){
-    return String(bytes/1024.0/1024.0)+"MB";
-  } else {
-    return String(bytes/1024.0/1024.0/1024.0)+"GB";
+String formatBytes(size_t bytes)
+{
+  if (bytes < 1024)
+  {
+    return String(bytes) + "B";
+  }
+  else if (bytes < (1024 * 1024))
+  {
+    return String(bytes / 1024.0) + "KB";
+  }
+  else if (bytes < (1024 * 1024 * 1024))
+  {
+    return String(bytes / 1024.0 / 1024.0) + "MB";
+  }
+  else
+  {
+    return String(bytes / 1024.0 / 1024.0 / 1024.0) + "GB";
   }
 }
 
-String getContentType(String filename){
-  if(server.hasArg("download")) return "application/octet-stream";
-  else if(filename.endsWith(".htm")) return "text/html";
-  else if(filename.endsWith(".html")) return "text/html";
-  else if(filename.endsWith(".css")) return "text/css";
-  else if(filename.endsWith(".js")) return "application/javascript";
-  else if(filename.endsWith(".png")) return "image/png";
-  else if(filename.endsWith(".gif")) return "image/gif";
-  else if(filename.endsWith(".jpg")) return "image/jpeg";
-  else if(filename.endsWith(".ico")) return "image/x-icon";
-  else if(filename.endsWith(".xml")) return "text/xml";
-  else if(filename.endsWith(".pdf")) return "application/x-pdf";
-  else if(filename.endsWith(".zip")) return "application/x-zip";
-  else if(filename.endsWith(".gz")) return "application/x-gzip";
+String getContentType(String filename)
+{
+  if (server.hasArg("download"))
+    return "application/octet-stream";
+  else if (filename.endsWith(".htm"))
+    return "text/html";
+  else if (filename.endsWith(".html"))
+    return "text/html";
+  else if (filename.endsWith(".css"))
+    return "text/css";
+  else if (filename.endsWith(".js"))
+    return "application/javascript";
+  else if (filename.endsWith(".png"))
+    return "image/png";
+  else if (filename.endsWith(".gif"))
+    return "image/gif";
+  else if (filename.endsWith(".jpg"))
+    return "image/jpeg";
+  else if (filename.endsWith(".ico"))
+    return "image/x-icon";
+  else if (filename.endsWith(".xml"))
+    return "text/xml";
+  else if (filename.endsWith(".pdf"))
+    return "application/x-pdf";
+  else if (filename.endsWith(".zip"))
+    return "application/x-zip";
+  else if (filename.endsWith(".gz"))
+    return "application/x-gzip";
   return "text/plain";
 }
 
-bool handleFileRead(String path){
+bool handleFileRead(String path)
+{
   Serial.println("handleFileRead: " + path);
-  if ((path == "/") && (server.argName(0) == "restart" && server.arg(0) == "true")) {
-        Serial.println(F("requested reset from admin page!"));
-        server.send(200, "text/plain", "Restarting!");
-        delay(200);
-        wdt_reset();ESP.restart();while (1) {wdt_reset();}
-        //WiFi.forceSleepBegin(); wdt_reset(); ESP.restart(); while (1)wdt_reset();
-      }
-  if(path.endsWith("/")) path += "index.htm";
+  if ((path == "/") && (server.argName(0) == "restart" && server.arg(0) == "true"))
+  {
+    Serial.println(F("requested reset from admin page!"));
+    server.send(200, "text/plain", "Restarting!");
+#ifdef NOT_DEBUG
+    digitalWrite(LED_BUILTIN, HIGH);
+#endif
+    delay(200);
+    wdt_reset();
+    ESP.restart();
+    while (1)
+    {
+      wdt_reset();
+    }
+    //WiFi.forceSleepBegin(); wdt_reset(); ESP.restart(); while (1)wdt_reset();
+  }
+  if (path.endsWith("/"))
+    path += "index.htm";
   String contentType = getContentType(path);
   String pathWithGz = path + ".gz";
-  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){
-    if(SPIFFS.exists(pathWithGz))
+  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path))
+  {
+    if (SPIFFS.exists(pathWithGz))
       path += ".gz";
     File file = SPIFFS.open(path, "r");
     size_t sent = server.streamFile(file, contentType);
@@ -355,50 +455,63 @@ bool handleFileRead(String path){
   return false;
 }
 
-void handleFileUpload(){
-  if(server.uri() != "/edit") return;
-  HTTPUpload& upload = server.upload();
-  if(upload.status == UPLOAD_FILE_START){
+void handleFileUpload()
+{
+  if (server.uri() != "/edit")
+    return;
+  HTTPUpload &upload = server.upload();
+  if (upload.status == UPLOAD_FILE_START)
+  {
     String filename = upload.filename;
-    if(!filename.startsWith("/")) filename = "/"+filename;
-    Serial.print("handleFileUpload Name: "); Serial.println(filename);
+    if (!filename.startsWith("/"))
+      filename = "/" + filename;
+    Serial.print("handleFileUpload Name: ");
+    Serial.println(filename);
     fsUploadFile = SPIFFS.open(filename, "w");
     filename = String();
-  } else if(upload.status == UPLOAD_FILE_WRITE){
+  }
+  else if (upload.status == UPLOAD_FILE_WRITE)
+  {
     //Serial.print("handleFileUpload Data: "); Serial.println(upload.currentSize);
-    if(fsUploadFile)
+    if (fsUploadFile)
       fsUploadFile.write(upload.buf, upload.currentSize);
-  } else if(upload.status == UPLOAD_FILE_END){
-    if(fsUploadFile)
+  }
+  else if (upload.status == UPLOAD_FILE_END)
+  {
+    if (fsUploadFile)
       fsUploadFile.close();
-    Serial.print("handleFileUpload Size: "); Serial.println(upload.totalSize);
+    Serial.print("handleFileUpload Size: ");
+    Serial.println(upload.totalSize);
   }
 }
 
-void handleFileDelete(){
-  if(server.args() == 0) return server.send(500, "text/plain", "BAD ARGS");
+void handleFileDelete()
+{
+  if (server.args() == 0)
+    return server.send(500, "text/plain", "BAD ARGS");
   String path = server.arg(0);
   Serial.println("handleFileDelete: " + path);
-  if(path == "/")
+  if (path == "/")
     return server.send(500, "text/plain", "BAD PATH");
-  if(!SPIFFS.exists(path))
+  if (!SPIFFS.exists(path))
     return server.send(404, "text/plain", "FileNotFound");
   SPIFFS.remove(path);
   server.send(200, "text/plain", "");
   path = String();
 }
 
-void handleFileCreate(){
-  if(server.args() == 0)
+void handleFileCreate()
+{
+  if (server.args() == 0)
     return server.send(500, "text/plain", "BAD ARGS");
   String path = server.arg(0);
   Serial.println("handleFileCreate: " + path);
-  if(path == "/")
+  if (path == "/")
     return server.send(500, "text/plain", "BAD PATH");
-  if(SPIFFS.exists(path))
+  if (SPIFFS.exists(path))
     return server.send(500, "text/plain", "FILE EXISTS");
   File file = SPIFFS.open(path, "w");
-  if(file)
+  if (file)
     file.close();
   else
     return server.send(500, "text/plain", "CREATE FAILED");
@@ -406,28 +519,34 @@ void handleFileCreate(){
   path = String();
 }
 
-void handleFileList() {
-  if(!server.hasArg("dir")) {server.send(500, "text/plain", "BAD ARGS"); return;}
-  
+void handleFileList()
+{
+  if (!server.hasArg("dir"))
+  {
+    server.send(500, "text/plain", "BAD ARGS");
+    return;
+  }
+
   String path = server.arg("dir");
   Serial.println("handleFileList: " + path);
   Dir dir = SPIFFS.openDir(path);
   path = String();
 
   String output = "[";
-  while(dir.next()){
+  while (dir.next())
+  {
     File entry = dir.openFile("r");
-    if (output != "[") output += ',';
+    if (output != "[")
+      output += ',';
     bool isDir = false;
     output += "{\"type\":\"";
-    output += (isDir)?"dir":"file";
+    output += (isDir) ? "dir" : "file";
     output += "\",\"name\":\"";
     output += String(entry.name()).substring(1);
     output += "\"}";
     entry.close();
   }
-  
+
   output += "]";
   server.send(200, "text/json", output);
 }
-
